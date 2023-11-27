c5ba6874d1c108b7ede396f7f13865ce4f9aeaf5
add <add@geosys.com>
2023-11-27 12:05:24 +0100

    End query games

--- server/src/app/v1/games/index.ts
+++ server/src/app/v1/games/index.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { poolManager } from '../../../postgresManager';
 
+type Game = {
+    id: number,
+    title: string,
+    returnDate: Date,
+    boolean: string
+}
+
 export class Games {
     //https://www.ludum.fr/blog/les-100-meilleurs-jeux-pour-jouer-en-ludotheque-n416#famille
     //document.querySelectorAll("span.blog_jeulist_info_title").forEach(retour = function(e){console.log(e.innerText);})
@@ -10,12 +17,14 @@ export class Games {
         const getGameList = (req: Request, res: Response, next: NextFunction) => {
             try {
                 poolManager.query('SELECT title, DATE(return_date), returned FROM cafeducoin.games ORDER BY title ASC')
-                    .then((result: any) => {                       
-                        res.send(result);
+                    .then((result) => {
+                        if (result == null)
+                            throw new Error('The result of the request is null.');
+                        res.send(result?.rows);
                     })
                     .catch((error: Error) => {
                         throw error;
-                    });              
+                    });
             } catch (error: unknown) {
                 next(error);
             }
@@ -30,12 +39,14 @@ export class Games {
             try {
                 const gameId = req.params.gameId;
                 poolManager.query('SELECT title, DATE(return_date), returned FROM cafeducoin.games WHERE id=$1', [gameId])
-                .then((result: any) => {                    
-                    res.send(result);
-                })
-                .catch((error: Error) => {
-                    throw error;
-                });             
+                    .then((result) => {
+                        if (result == null)
+                            throw new Error('The result of the request is null.');
+                        res.send(result?.rows);
+                    })
+                    .catch((error: Error) => {
+                        throw error;
+                    });
             } catch (error: unknown) {
                 next(error);
             }
@@ -50,7 +61,17 @@ export class Games {
         const updateGame = (req: Request, res: Response, next: NextFunction) => {
             try {
                 const gameId = req.params.gameId;
-                res.send(`Update du jeu ${gameId}`);
+                poolManager.query(`UPDATE cafeducoin.games
+                                   SET returned= NOT returned
+                                   WHERE id=$1`, [gameId])
+                    .then((result) => {
+                        if (result == null)
+                            throw new Error('The result of the request is null.');
+                        res.send(result?.rows);
+                    })
+                    .catch((error: Error) => {
+                        throw error;
+                    });
             } catch (error: unknown) {
                 next(error);
             }
